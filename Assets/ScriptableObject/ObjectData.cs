8875ad694761c596fe854b14556726ea7318b8bd
ACEDIA2567 <sdman2567@gmail.com>
2024-06-17 17:01:36 +0900

    Feat: 상호작용할 아이템 추가
    플레이어가 상호작용 해야하는 아이템에 대한 값 설정

--- Assets/ScriptableObject/ObjectData.cs
+++ Assets/ScriptableObject/ObjectData.cs
@@ -2,8 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
+[CreateAssetMenu(fileName = "Object Data", menuName = "Scriptable/Object", order = 0)]
 public class ObjectData : ScriptableObject
 {
-    
+    public string objectName; // 물건 이름
+    public Sprite sprite;     // 이미지
+    public int count;         // 횟수
+    public int gold;          // 골드 량
 }
